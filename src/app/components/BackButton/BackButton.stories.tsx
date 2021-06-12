227f3eb4f8b53f127073eaa1bf83b682b29b9cb5
dhnazarenko <danielnazarenko.de@gmail.com>
2021-06-12 11:57:21 +0200

    :sparkles: Add Back Button Component

---
+++ src/app/components/BackButton/BackButton.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BackButton from './BackButton';
+import BackButtonIcon from '../Icons/BackButtonIcon';
+
+export default {
+  title: 'Component/BackButton',
+  component: BackButton,
+};
+
+export const Back = (): JSX.Element => <BackButton icon={<BackButtonIcon />} />;
