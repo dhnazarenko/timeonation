523786ec023738d1f0b7fce3e0f7741b1c8fac0c
dhnazarenko <danielnazarenko.de@gmail.com>
2021-06-12 14:30:30 +0200

    :recycle: Change Import BackButtonIcon

--- src/app/components/BackButton/BackButton.stories.tsx
+++ src/app/components/BackButton/BackButton.stories.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import BackButton from './BackButton';
-import BackButtonIcon from '../Icons/BackButtonIcon';
 
 export default {
   title: 'Component/BackButton',
   component: BackButton,
 };
 
-export const Back = (): JSX.Element => <BackButton icon={<BackButtonIcon />} />;
+export const Back = (): JSX.Element => <BackButton />;
