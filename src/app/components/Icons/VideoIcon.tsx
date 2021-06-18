bd892ec886eb15c7349f13e3be38784ae60683cb
dhnazarenko <danielnazarenko.de@gmail.com>
2021-06-18 13:57:43 +0200

    :sparkles: Add video button component

---
+++ src/app/components/Icons/VideoIcon.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function VideoIcon(): JSX.Element {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="36"
+      height="24"
+      viewBox="0 0 36 24"
+    >
+      <g
+        id="Icon_feather-video"
+        data-name="Icon feather-video"
+        transform="translate(0 -6)"
+      >
+        <path
+          id="Pfad_256"
+          data-name="Pfad 256"
+          d="M34.5,10.5,24,18l10.5,7.5Z"
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="3"
+        />
+        <path
+          id="Pfad_257"
+          data-name="Pfad 257"
+          d="M4.5,7.5H21a3,3,0,0,1,3,3v15a3,3,0,0,1-3,3H4.5a3,3,0,0,1-3-3v-15a3,3,0,0,1,3-3Z"
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="3"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default VideoIcon;
