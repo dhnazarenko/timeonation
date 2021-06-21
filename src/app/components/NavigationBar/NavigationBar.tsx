b702606692fe62d9f58516aaf9b8128d812a7f29
dhnazarenko <danielnazarenko.de@gmail.com>
2021-06-21 13:40:11 +0200

    :sparkles: Add navigation bar component

---
+++ src/app/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styles from './NavigationBar.module.css';
+import NavigationLink from '../NavigationLink/NavigationLink';
+import { useLocation } from 'react-router-dom';
+import HomeIcon from '../Icons/HomeIcon';
+import NavUserIcon from '../Icons/NavUserIcon';
+import NavSearchIcon from '../Icons/NavSearchIcon';
+
+export default function NavBar(): JSX.Element {
+  const location = useLocation();
+  const activePath: string = location.pathname;
+
+  return (
+    <div className={styles.navigationBar}>
+      <NavigationLink
+        active={activePath === '/home'}
+        icon={<HomeIcon />}
+        link="/home"
+      />
+      <NavigationLink
+        active={activePath === '/search'}
+        icon={<NavSearchIcon />}
+        link="/search"
+      />
+
+      <NavigationLink
+        active={activePath === '/profile'}
+        icon={<NavUserIcon />}
+        link="/profile"
+      />
+    </div>
+  );
+}
