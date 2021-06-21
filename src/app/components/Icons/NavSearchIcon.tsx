c17801622c5b70dfdaeb4e661b2bad425bf4cbb9
dhnazarenko <danielnazarenko.de@gmail.com>
2021-06-21 12:50:56 +0200

    :sparkles: Add icons for navigation bar

---
+++ src/app/components/Icons/NavSearchIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NavSearchIcon(): JSX.Element {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34.078 34.078">
+      <path
+        d="M26.376,21.841a13.477,13.477,0,1,0-4.537,4.533l9.144,9.143,4.535-4.537Zm-11.49,1.364A8.314,8.314,0,1,1,23.2,14.9,8.327,8.327,0,0,1,14.886,23.205Z"
+        transform="translate(-1.44 -1.44)"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
+
+export default NavSearchIcon;
