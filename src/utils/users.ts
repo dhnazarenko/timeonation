2d5e26443e5a6b2e9fe2c7749aaf1ba6b66d57c3
dhnazarenko <danielnazarenko.de@gmail.com>
2021-06-22 18:18:04 +0200

    :sparkles: Add readUsers readUser saveUser function

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -1,10 +1,18 @@
 import type { User } from '../types';
 import { getUsersCollection } from './database';
 
-export const saveUser = async (user: User): Promise<void> => {
-  await getUsersCollection().insertOne({ ...user });
+export const readUsers = async (): Promise<User[]> => {
+  return await getUsersCollection().find().toArray();
 };
 
-export const readUsers = async (): Promise<User[]> => {
-  return await getUsersCollection().find().sort({ email: 1 }).toArray();
+export const readUser = async (email: string): Promise<User | undefined> => {
+  const user = await getUsersCollection().findOne({ email });
+  if (!user) {
+    throw new Error('There is no user with that email adress');
+  }
+  return user;
+};
+
+export const saveUser = async (newUser: User): Promise<void> => {
+  await getUsersCollection().insertOne({ ...newUser });
 };
