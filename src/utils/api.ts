7ec9eb812918e5847d936cb0a000bc8f5a81501f
dhnazarenko <danielnazarenko.de@gmail.com>
2021-06-23 15:06:05 +0200

    :sparkles: Add fetch postLoginUser

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,3 +15,19 @@ export async function postUser(user: User): Promise<User> {
   const result: User = await response.json();
   return result;
 }
+
+export async function postLoginUser(user: Partial<User>): Promise<User> {
+  const response = await fetch('/api/users/Login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  });
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    throw errorMessage;
+  }
+  const result: User = await response.json();
+  return result;
+}
