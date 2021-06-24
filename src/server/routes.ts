7212cae88bd7514e1eea5aa0bea0b7a759e9e192
dhnazarenko <danielnazarenko.de@gmail.com>
2021-06-24 12:19:13 +0200

    :sparkles: Set cookie

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -22,6 +22,11 @@ router.post('/users/login', async (req, res, next) => {
       return;
     }
 
+    res.setHeader(
+      'Set-Cookie',
+      `userId=${user._id};path=/;Max-Age=${365 * 24 * 60 * 60}`
+    );
+
     res.status(200).json(user);
   } catch (error) {
     next(error);
