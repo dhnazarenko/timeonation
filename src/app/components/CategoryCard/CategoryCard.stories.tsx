17cd0b3cb39d9cdf461d6b374a21b8bc8482b9c8
dhnazarenko <danielnazarenko.de@gmail.com>
2021-07-01 15:41:24 +0200

    :sparkles: Add categorycard map array and add link

--- src/app/components/CategoryCard/CategoryCard.stories.tsx
+++ src/app/components/CategoryCard/CategoryCard.stories.tsx
@@ -6,13 +6,37 @@ export default {
   component: CategoryCard,
 };
 
-export const Klimaschutz = (): JSX.Element => (
-  <CategoryCard categoryTitle="Klimaschutz" />
-);
-export const Nothilfe = (): JSX.Element => (
-  <CategoryCard categoryTitle="Nothilfe Coronavirus" />
+const categoriesArray = [
+  'Klimaschutz',
+  'Tiere',
+  'Kinder und Jugend',
+  'Bildung',
+];
+
+export const showCategory1 = (): JSX.Element => (
+  <CategoryCard categories={categoriesArray} />
 );
 
-export const Syrien = (): JSX.Element => (
-  <CategoryCard categoryTitle="Syrien Nothilfe" />
+const categoriesArray2 = [
+  'Geflüchtete',
+  'Umwelt',
+  'Frauen',
+  'Gesundheit',
+  'Kultur',
+];
+
+export const showCategory2 = (): JSX.Element => (
+  <CategoryCard categories={categoriesArray2} />
+);
+
+const categoriesArray3 = [
+  'Katastrophenschutz',
+  'Nothilfe Beirut',
+  'Seenotrettung',
+  'Ernährung',
+  'Obdachlosenhilfe',
+];
+
+export const showCategory3 = (): JSX.Element => (
+  <CategoryCard categories={categoriesArray3} />
 );
